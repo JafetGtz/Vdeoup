12b9495b8e7b29104fe3b63ff23e32d13a31d8a5
JafetGtz <gtzjafet@gmail.com>
2023-02-25 14:24:07 -0600

    delete libraries errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import TabNavigator from './TabNavigator';
 import {NavigationContainer} from '@react-navigation/native';
-import { Comentarios } from './comments';
 import Route from './navigation';
 export default () => {
   return (
-    <Comentarios/>
+    <NavigationContainer>
+      <TabNavigator />
+    </NavigationContainer>
   );
 };
